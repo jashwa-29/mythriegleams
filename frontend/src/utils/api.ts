b900147cdb068a615eea90fb218701d1f8b15d77
jashwa-29 <jashwa46733@gmail.com>
2026-07-29 17:04:10 +0530

    gallery changes

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'
+    baseURL: process.env.NEXT_PUBLIC_API_URL
 });
 
 // Add a request interceptor to attach JWT token
